bb708e94099480077dd657f92d2b4725587b9705
tfrancis5 <112411889+tfrancis5@users.noreply.github.com>
2022-09-08 12:18:33 -0400

    Add files via upload

---
+++ BUaddress.java
@@ -0,0 +1,6 @@
+public class BUaddress
+{
+public static void main (String[]args)
+{
+System.out.println("Bellarmine University \n2001 Newburb Road \nLouisville, Kentucky 40205");
+}}
---
+++ MyMondaySchedule.java
@@ -0,0 +1,15 @@
+public class MyMondaySchedule
+{
+public static void main (String[]args)
+{
+System.out.println("On Monday I wake up at"+9+"AM");
+System.out.println("I either eat breakfast in my dorm or eat breakfast at Palio");
+System.out.println("Next I go to my accounting class at"+10+"AM");
+System.out.println("Then I come to programming fundamentals class");
+System.out.println("After class I go get some lunch");
+System.out.println("After lunch I go back to my dorm and do homework and then take a nap");
+System.out.println("I nap until "+3+" and then get ready for football");
+System.out.println("I go to football practice and get back to campus at "+7);
+System.out.println("I shower and eat dinner then study and finish any homework");
+System.out.println("To end my Monday I go to sleep around "+12);
+}}
